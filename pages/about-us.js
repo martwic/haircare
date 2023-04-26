37f7b88404dfc3f9e4d36f23eb0fc0dd5fddfe26
Marta Wichłacz <martwic389@student.polsl.pl>
2023-04-26 13:53:26 +0200

    about

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,13 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
+
 import Link from 'next/link';
 
-export default function Home({data}) {
+export default function Home({data}){
   return (
-    <div>
-      <div class='main'>
-        
-      </div>
-    </div>
+    <div className='bodyLog'>
+      <div className='mainLog'>
+      <div className="sectionLog">
+                <div className="boxOfProducts">
+                <p>Jesteśmy grupą dziewczyn, które uwielbiają dbać o swoje włosy i chcą pomóc innym w
+                osiągnięciu najlepszego efektu pielęgnacji. Nasza strona HairCare oferuje
+                spersonalizowane porady dotyczące pielęgnacji włosów, które pomogą Ci znaleźć
+                kosmetyki najlepiej dopasowane do Twojego konkretnego przypadku.
+                Nie jesteśmy związane z żadną konkretną marką kosmetyków - kierujemy się wyłącznie
+                składem i działaniem produktów, aby móc polecić Wam najlepsze rozwiązania dla
+                Waszych włosów. Jesteśmy przekonane, że każda osoba zasługuje na zdrowe i piękne
+                włosy. Dlatego stworzyłyśmy HairCare - aby pomóc Ci w osiągnięciu tego celu.
+                Nasza metoda działania jest prosta: wypełnij ankietę dotyczącą Twoich włosów, a my
+                przygotujemy spersonalizowaną listę kosmetyków, które najlepiej dopasują się do
+                Twojego typu włosów i potrzeb pielęgnacyjnych. Dzięki temu oszczędzisz czas i
+                pieniądze, unikając próbowania wielu różnych produktów, zanim znajdziesz ten idealny.
+                Naszą misją jest pomaganie innym w pielęgnacji włosów i dzielenie się naszą wiedzą
+                oraz doświadczeniem. Zapraszamy do śledzenia naszych porad na stronie internetowej
+                oraz w mediach społecznościowych. Jeśli masz pytania lub potrzebujesz porady,
+                skontaktuj się z nami - chętnie pomożemy!</p>
+  </div>
+  </div>
+  </div>
+  </div>
   )
 }
+
