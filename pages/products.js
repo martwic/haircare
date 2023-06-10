2064213cebdfe95b33f053f3e93377700d0e668a
martwic <72266358+martwic@users.noreply.github.com>
2023-06-10 02:36:45 +0200

    search

--- pages/products.js
+++ pages/products.js
@@ -7,8 +7,46 @@ export default function Home({products}){
       <div className='mainLog'>
       <div className="sectionLog">
                 <div className="boxOfProducts">
+                <div className='search-bar'>
+          <div id="search-container">
+            <div className='select-search'>
+              <input
+            type="search"
+            id="search-input"
+            placeholder="Wyszukaj nazwę produktu..."
+            className='search-box'
+              />
+              <div className='label-select-container'>
+              <label htmlfor="typ-produktu" className="label-produkt">Typ produktu: </label>
+                <select id="typ-produktu" name="typ-produktu" className="filter-select">
+                  <option value="">Wszystkie</option>
+                  <option value="szampon">Szampon</option>
+                  <option value="odzywka">Odżywka</option>
+                  <option value="maska">Maska</option>
+                  <option value="serum">Serum</option>
+                  <option value="wcierka">Wcierka</option>
+                  <option value="peeling">Peeling</option>
+                </select>
+              </div>
+              
+              <div className='label-select-container'>
+                <label htmlfor="typ-wlosow" class="label-produkt">Typ włosów: </label>
+                <select id="typ-produktu" name="typ-produktu" className="filter-select">
+                  <option value="">Wszystkie</option>
+                  <option value="niskoporowate">Niskoporowate</option>
+                  <option value="srednioporowate">Średnioporotwate</option>
+                  <option value="wysokoporowate">Wysokoporowate</option>
+                </select>
+                
+              </div>
+              <button id="search" className="button2">Szukaj</button>
+          </div>
+
+          </div>
+          </div>
     <table>
     <tbody>
+
     {products.map((produkty) => (
       <tr key={produkty.id_produktu}>
         <td><img alt="zdjprod" src={`/images/products//${produkty.id_produktu}.png`} /></td>
